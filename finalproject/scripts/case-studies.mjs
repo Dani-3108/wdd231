44ec83abe70f96d2ffd7d8b64f77ec6da7694894
Dani-3108 <floresd477@gmail.com>
2026-06-14 21:50:54 -0600

    Week 06: Final project part 1.6

---
+++ finalproject/scripts/case-studies.mjs
@@ -0,0 +1,20 @@
+//Footer section that repeats in each page
+const today = new Date();
+document.getElementById("currentyear").innerHTML = today.getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
+//Hamburger button section for menu in navegation bar
+const button = document.querySelector("#hamburger");
+const navigation = document.querySelector(".menu");
+//listener events for menu button
+button.addEventListener("click", function () {
+    if (button.classList.contains("open")) {
+        button.innerHTML = "&#9776";
+    }
+    else {
+        button.innerHTML = "&#10005";
+    }
+    navigation.classList.toggle("open");
+    button.classList.toggle("open");
+});
+
+/////////////////
