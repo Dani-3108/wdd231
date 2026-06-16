54dc4d20c8ccfe18ea447dde44fd0daf0abc9920
Dani-3108 <floresd477@gmail.com>
2026-06-16 00:59:20 -0600

    Week 06: case studies pages finished

--- finalproject/scripts/case-studies.mjs
+++ finalproject/scripts/case-studies.mjs
@@ -1,20 +1,40 @@
-//Footer section that repeats in each page
-const today = new Date();
-document.getElementById("currentyear").innerHTML = today.getFullYear();
-document.getElementById("lastModified").innerHTML = document.lastModified;
-//Hamburger button section for menu in navegation bar
-const button = document.querySelector("#hamburger");
-const navigation = document.querySelector(".menu");
-//listener events for menu button
-button.addEventListener("click", function () {
-    if (button.classList.contains("open")) {
-        button.innerHTML = "&#9776";
+//variables
+const CaseStudiesArray = "data/case-studies.json";
+const CaseStudiesCards = document.querySelector("#case-studies-cards");
+//function to create cards and append the information to it
+const DisplayCaseStudies = (caseStudies) => {
+    caseStudies.forEach((caseStudie) => {
+        //creating elements
+        let card = document.createElement("section");
+        card.innerHTML = `
+            <div>
+                <h2>${caseStudie.title}</h2>
+                <img src="images/${caseStudie.image}" alt="${caseStudie.title}" loading="lazy">
+                <ul>
+                    <li>${caseStudie.description}</li>
+                    <li class="key-metric">${caseStudie.keyMetric}</li>
+                    <li>Industry: ${caseStudie.industry}</li>
+                </ul>
+            </div>`;
+        //appending elements
+        CaseStudiesCards.appendChild(card);
+    });
+}
+//Function to get data from json file
+async function getCaseStudies() {
+    try {
+        const response = await fetch(CaseStudiesArray);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            DisplayCaseStudies(data.caseStudies);
+        }
+        else {
+            throw Error(await response.text());
+        }
     }
-    else {
-        button.innerHTML = "&#10005";
+    catch (error){
+        console.log(error);
     }
-    navigation.classList.toggle("open");
-    button.classList.toggle("open");
-});
-
-/////////////////
+}
+getCaseStudies();
